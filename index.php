ed8d88bd0fa38be21b43e5aae99adb97779c4089
Falmban Ammar <devAspirant@gmail.com>
2021-04-21 16:35:54 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,10 +1,46 @@
 <!-- header.php استدعاء ملف -->
 <?php
 $title = 'Home Page';
-require_once 'template/header.php' ?>
+require_once 'template/header.php';
 
+//  اظهار المعلومات في اعلى الصفحة عشان نعرف البيانات -->
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+   /* echo "<pre>";
+    print_r($_POST);
+    print_r($_FILES);
+    echo "</pre>";*/
+echo "entered";
+    /* اعداد الملفات المسموح برفعها و نتاكد انه الملف نظيف  */
+    if(isset($_FILES['document'])){
+        echo 'document';
+    }
+    
+    if(isset($_FILES['document']) && $_FILES['document']['error'] == 0){
+        
+        echo "File is fine";
+
+        /* نوع الملفات المسموح رفعها  */
+        /*$allowed = [
+           // 'jpg' => 'image/jpeg',
+           // 'png' => 'image/png',
+            //'gif' => 'image/gif'
+        ];
+
+        $fileType = $_FILES['document']['type'];
+        echo $fileType;
+        
+
+        if(!in_array($fileType,$allowed)){
+            echo 'File type not allowed';
+            die();
+        }*/
+        
+    }
+}
+?>
 <!-- create form contact -->
-<form action="" method="post">
+<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" enctype="multipart/form-data">
     <div class="form-group">
         <label for="name">your name</label>
         <input type="text" name="name" class="form-control" placeholder="your name"></input>
